be750ceb1243ded3f2d41dd44fe9c08699ae543f
Dankshaman <72111094+Dankshaman@users.noreply.github.com>
2025-08-29 02:17:23 -0500

    Update ED_AP.py

--- ED_AP.py
+++ ED_AP.py
@@ -34,7 +34,6 @@ from Voice import *
 from Robigo import *
 from TCE_Integration import TceIntegration
 from EDFleetCarrierAP import FleetCarrierAutopilot
-from WingMining import WingMining
 
 """
 File:EDAP.py    EDAutopilot
@@ -109,17 +108,6 @@ class EDAutopilot:
             "EDMesgActionsPort": 15570,
             "EDMesgEventsPort": 15571,
             "DebugOverlay": False,
-            "WingMining_StationA": "",
-            "WingMining_StationB": "",
-            "WingMining_FC_A_Bertrandite": "",
-            "WingMining_FC_A_Gold": "",
-            "WingMining_FC_A_Indite": "",
-            "WingMining_FC_A_Silver": "",
-            "WingMining_FC_B_Bertrandite": "",
-            "WingMining_FC_B_Gold": "",
-            "WingMining_FC_B_Indite": "",
-            "WingMining_FC_B_Silver": "",
-            "WingMining_CompletedMissions": 0,
         }
         # NOTE!!! When adding a new config value above, add the same after read_config() to set
         # a default value or an error will occur reading the new value!
@@ -168,28 +156,6 @@ class EDAutopilot:
                 cnf['EDMesgEventsPort'] = 15571
             if 'DebugOverlay' not in cnf:
                 cnf['DebugOverlay'] = False
-            if 'WingMining_StationA' not in cnf:
-                cnf['WingMining_StationA'] = ""
-            if 'WingMining_StationB' not in cnf:
-                cnf['WingMining_StationB'] = ""
-            if 'WingMining_FC_A_Bertrandite' not in cnf:
-                cnf['WingMining_FC_A_Bertrandite'] = ""
-            if 'WingMining_FC_A_Gold' not in cnf:
-                cnf['WingMining_FC_A_Gold'] = ""
-            if 'WingMining_FC_A_Indite' not in cnf:
-                cnf['WingMining_FC_A_Indite'] = ""
-            if 'WingMining_FC_A_Silver' not in cnf:
-                cnf['WingMining_FC_A_Silver'] = ""
-            if 'WingMining_FC_B_Bertrandite' not in cnf:
-                cnf['WingMining_FC_B_Bertrandite'] = ""
-            if 'WingMining_FC_B_Gold' not in cnf:
-                cnf['WingMining_FC_B_Gold'] = ""
-            if 'WingMining_FC_B_Indite' not in cnf:
-                cnf['WingMining_FC_B_Indite'] = ""
-            if 'WingMining_FC_B_Silver' not in cnf:
-                cnf['WingMining_FC_B_Silver'] = ""
-            if 'WingMining_CompletedMissions' not in cnf:
-                cnf['WingMining_CompletedMissions'] = 0
             self.config = cnf
             logger.debug("read AP json:"+str(cnf))
         else:
@@ -237,8 +203,6 @@ class EDAutopilot:
         self.dss_assist_enabled = False
         self.single_waypoint_enabled = False
         self.fc_assist_enabled = False
-        self.wing_mining_assist_enabled = False
-        self.wing_mining_next_state = None
 
         # Create instance of each of the needed Classes
         self.gfx_settings = EDGraphicsSettings()
@@ -257,7 +221,6 @@ class EDAutopilot:
         self.ap_ckb = cb
         self.waypoint = EDWayPoint(self, self.jn.ship_state()['odyssey'])
         self.robigo = Robigo(self)
-        self.wing_mining = WingMining(self)
         self.status = StatusParser()
         self.nav_route = NavRouteParser()
         self.ship_control = EDShipControl(self, self.scr, self.keys, cb)
@@ -2307,11 +2270,21 @@ class EDAutopilot:
             if res is False:
                 return False
 
-        # Disabled until SC to station enabled.
-        # if self._single_waypoint_station != "":
-        #     res = self.supercruise_to_station(self.scrReg, self._single_waypoint_station)
-        #     if res is False:
-        #         return False
+        if self._single_waypoint_station != "":
+            station_to_find = self._single_waypoint_station
+            if "EXT_PANEL_ColonisationShip;".upper() in station_to_find.upper():
+                parts = station_to_find.split(';')
+                if len(parts) > 1:
+                    station_to_find = parts[1].strip()
+
+            res = self.nav_panel.select_station_by_ocr(station_to_find)
+            if not res:
+                self.ap_ckb('log+vce', f"Unable to set station by Nav-OCR.")
+                return False
+
+            res = self.supercruise_to_station(self.scrReg, self._single_waypoint_station)
+            if res is False:
+                return False
 
     # raising an exception to the engine loop thread, so we can terminate its execution
     #  if thread was in a sleep, the exception seems to not be delivered
@@ -2383,18 +2356,6 @@ class EDAutopilot:
         if enable == False and self.fc_assist_enabled == True:
             self.ctype_async_raise(self.ap_thread, EDAP_Interrupt)
         self.fc_assist_enabled = enable
-
-    def set_wing_mining_assist(self, enable=True):
-        if enable:
-            if not self.wing_mining_assist_enabled:
-                self.wing_mining.start()
-        else:
-            if self.wing_mining_assist_enabled:
-                self.wing_mining.stop()
-        self.wing_mining_assist_enabled = enable
-
-    def reset_wing_mining_counter(self):
-        self.wing_mining.reset_mission_counter()
 
     def set_cv_view(self, enable=True, x=0, y=0):
         self.cv_view = enable
@@ -2605,14 +2566,6 @@ class EDAutopilot:
                 self.fc_assist_enabled = False
                 self.ap_ckb('fc_stop') # This will need to be added to the GUI
                 self.update_overlay()
-
-            elif self.wing_mining_assist_enabled == True:
-                try:
-                    self.wing_mining.run()
-                except Exception as e:
-                    logger.error(f"Error in Wing Mining assist: {e}")
-                    traceback.print_exc()
-                    self.set_wing_mining_assist(False) # Stop on error
 
             # Check once EDAPGUI loaded to prevent errors logging to the listbox before loaded
             if self.gui_loaded:
